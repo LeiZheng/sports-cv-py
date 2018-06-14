54bb870e005b5bc49d4e423cd02b2da3849e0669
Lei Zheng <zhenglei2010fall@gmail.com>
2018-06-14 19:20:04 -0400

    kalman impl

---
+++ detectai/Player.py
@@ -0,0 +1,53 @@
+import cv2
+import numpy as np
+class Player():
+    def buildKanmanFilter(self):
+        filter = cv2.KalmanFilter(4,2)
+        filter.measurementMatrix = np.array([[1,0,0,0],[0,1,0,0]],np.float32)
+        filter.transitionMatrix = np.array([[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]],np.float32)
+        filter.processNoiseCov =filter.transitionMatrix *0.03
+        return filter
+
+    def __init__(self, name, frame, windows):
+        self.name = name;
+        x, y, w, h = windows;
+        self.roi = cv2.cvtColor(frame[y:y + h, x:x+w], cv2.COLOR_BGR2HSV)
+        roi_hist = cv2.calcHist([self.roi], [0],None, [16],[0, 180])
+        self.roi_hist = cv2.normalize(roi_hist, roi_hist, 0, 255, cv2.NORM_MINMAX)
+
+        # set up the kalman for forecast
+        self.kalman = self.buildKanmanFilter()
+        self.measurement = np.array((2,1), np.float32)
+        self.prediction = np.zeros((2,1), np.float32)
+        self.term_crit = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1)
+        self.center = None
+        self.track_window = windows
+        self.update(frame)
+
+    def __del__(self):
+        print("Player %s destroyed" % self.name)
+
+
+    def update(self, frame):
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        back_project = cv2.calcBackProject([hsv],[0],self.roi_hist,  [0, 180],1)
+        ret, self.track_window = cv2.CamShift(back_project, self.track_window, self.term_crit)
+        pts = cv2.boxPoints(ret)
+        pts = np.int0(pts)
+        x, y, w, h = cv2.boundingRect(pts)
+        self.center = [[x + w/2], [y+h/2]]
+        cv2.polylines(frame, [pts], True, 255,1)
+        self.kalman.correct([self.center])
+
+        prediction = self.kalman.predict()
+        cv2.circle(frame, (int(prediction[0]), int(prediction[1])), 4, (0,255,0), -1)
+
+        cv2.putText(frame, "Name: %s -> %s" (self.name, self.center), 'Sport Max', cv2.QT_FONT_NORMAL, 0.6, (0,0,0),1, cv2.LINE_AA )
+
+        cv2.putText(frame, "Name: %s -> %s"(self.name, self.center), 'Sport Max', cv2.QT_FONT_NORMAL, 0.6, (0, 255, 0), 1, cv2.LINE_AA)
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,2 +1,53 @@
+import cv2
+from detectai.Player import Player
 
-print('main')
+history = 20
+bsKnn = cv2.createBackgroundSubtractorKNN(detectShadows=True)
+bsKnn.setHistory(history)
+
+cv2.namedWindow("tracing players")
+pedestrians = []
+firstFrame = True
+frames = 0
+
+
+cap = cv2.VideoCapture("C:/vsc/data/hockey-tracking/player.avi")
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if ret is False:
+        print("exit the video play")
+        break
+
+    fgmask = bsKnn.apply(frame)
+
+    if frames < history:
+        frames += 1
+        continue
+
+    th = cv2.threshold(fgmask.copy(), 127, 255, cv2.THRESH_BINARY)[1]
+    th = cv2.erode(th, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3)), iterations=2)
+    dilated = cv2.dilate(th, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (8, 3)), iterations=2)
+    image, contours, hier = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    counter = 0
+    for c in contours:
+        if cv2.contourArea(c) > 500:
+            (x, y, w, h) = cv2.boundingRect(c)
+            cv2.rectangle(frame, (x,y), (x +w, y + h), (0,255, 0), 1)
+            if firstFrame is True:
+                pedestrians[counter] = Player("player " + str(counter), frame, (x, y, w,h))
+            counter += 1
+
+
+    for i, p in pedestrians:
+        p.update(frame)
+
+    firstFrame = False
+
+    frames += 1
+    cv2.imshow("tracing players", frame)
+    cv2.waitKey(10)
+
+
+
